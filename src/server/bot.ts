b9aa6cf2134fad5f248b8484416e001bf372e424
YOSHENAN A/L SHANKER <yosh45650@gmail.com>
2026-09-24 19:25:59 +0800

    Update API endpoint URL in bot.ts

--- src/server/bot.ts
+++ src/server/bot.ts
@@ -137,7 +137,7 @@ const requestWizard = new Scenes.WizardScene<Scenes.WizardContext>(
       try {
 
         const response = await fetch(
-          '/api/submission',
+          'https://landingpage-rq72.onrender.com/api/submission',
           {
             method: 'POST',
             headers: {
