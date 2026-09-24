08be56f3498f94d8b2e38cfa50ccaea841f9131b
YOSHENAN A/L SHANKER <yosh45650@gmail.com>
2026-09-24 18:48:58 +0800

    Update db.js

--- dist/db.js
+++ dist/db.js
@@ -46,7 +46,7 @@ const telegramSchema = new mongoose.Schema({
 });
 export const User = mongoose.model('User', userSchema);
 export const Log = mongoose.model('Log', logSchema);
-export const Telegram = mongoose.model('Telegram', telegramSchema, 'telegrams');
+export const Telegram = mongoose.model('Telegram', telegramSchema);
 // 4. Database Connection Function
 export const connectDB = async () => {
     try {
